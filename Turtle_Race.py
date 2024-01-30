871266b21eb5c0ea0aac8260f40c092392c653c8
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-01-30 17:00:09 +0300

    Add random_movement method for moving turtles forward randomly. Fix the bug that cause making turtles were same color.

--- Turtle_Race.py
+++ Turtle_Race.py
@@ -7,33 +7,36 @@ scrn.setup(width=0.75, height=0.75, startx=None, starty=None)
 
 selected_turtle = scrn.textinput(title="Turtle Race!", prompt="Please choise your turtle.")
 
-
-# def random_color():
-#     r = random.randint(1, 255)
-#     b = random.randint(1, 255)
-#     g = random.randint(1, 255)
-#     new_color = (r, b, g)
-#     return new_color
-
 colors = ["red", "cyan", "black", "blue", "magenta", "white",
-          "yellow", "silver", "pink", "gold", "grey", "aguamarine",
+          "yellow", "silver", "pink", "gold", "grey", "aquamarine",
           "violet", "red3", "purple", "aqua", "lime", "gold3", "plum"]
 
 
 def create_turtles(loc):
-    for x in range(10):
-        x = t.Turtle()
-        x.shape("turtle")
+    racers = []
+    for racer in range(10):
+        racer = t.Turtle()
+        racer.shape("turtle")
         r_choise = random.choice(colors)
-        x.color(r_choise)
+        if r_choise == "":
+            r_choise = random.choice(colors)
+        racer.color(r_choise)
         colors.remove(r_choise)
-        x.shapesize(2, 2, 3)
-        x.penup()
-        x.goto(x=-600, y=loc)
+        racer.shapesize(2, 2, 3)
+        racer.penup()
+        racer.goto(x=-600, y=loc)
         loc += 60
+        racers.append(racer)
+    return racers
 
 
+def random_movement():
+    turtles = create_turtles(-300)
+    for _ in range(200):
+        turtle = random.choice(turtles)
+        turtle.forward(100)
 
-create_turtles(-300)
+
+random_movement()
 
 scrn.exitonclick()
